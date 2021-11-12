19dacc1bb976009059ed82a8ba2d18775213f93f
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2021-11-12 17:03:30 +0100

    Have successfully tested the default and the docker maven profile

--- fuse/infra/docker-start-jboss-fuse.sh
+++ fuse/infra/docker-start-jboss-fuse.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 function waiting_for_jboss {
-  while [[ $(sudo netstat -ltnp | grep 9990 | wc -l) -lt 1 ]] ; do sleep 1s ; done
+  while [[ $(sudo netstat -ltnp | grep 9990 | wc -l) -lt 1 ]]
+  do
+    sleep 5s
+  done
 }
 
 function download_files() {
@@ -11,29 +14,33 @@ function download_files() {
 
 if [[ ! -e $EAP_HOME/bin && -e /root/install ]]; then
   cd $EAP_HOME
-  echo "Downloading JBoss installation files ..."
+  echo ">>> Downloading JBoss installation files ..."
   download_files
-  echo "Installing JBoss EAP 7.4.0 ..."
+  echo ">>> Installing JBoss EAP 7.4.0 ..."
   java -jar /root/install/jboss-eap-7.4.0-installer.jar /root/install/auto.xml -variablefile /root/install/auto.xml.variables
   if [[ "0" != "$?" ]]; then
-    echo "Error installing JBoss EAP 7.4.0"
+    echo "### Error installing JBoss EAP 7.4.0"
     exit 1
   fi
-  echo "Patching JBoss EAP 7.4.1 ..."
+  echo ">>> Patching JBoss EAP 7.4.1 ..."
   echo "connect
   patch apply /root/install/jboss-eap-7.4.1-patch.zip
-  shutdown --restart=true" > patch.cli
-  $EAP_HOME/bin/standalone.sh
+  shutdown --restart=false" > patch.cli
+  $EAP_HOME/bin/standalone.sh &
   waiting_for_jboss
-  jboss-cli.sh --file=patch.cli
-  waiting_for_jboss
+  $EAP_HOME/bin/jboss-cli.sh --file=patch.cli
   rm -rf patch.cli
-  echo "Installing JBoss Fuse 7.9.0 ..."
-  java -jar /root/install/fuse-eap-installer-7.9.0.redhat-187.jar
+  echo ">>> Installing JBoss Fuse 7.9.0 ..."
+  java -jar /root/install/fuse-eap-installer-7.9.0.jar
   if [[ "0" != "$?" ]]; then
-    echo "Error installing JBoss Fuse 7.9.0"
+    echo "### Error installing JBoss Fuse 7.9.0"
     exit 1
   fi
-  echo "JBoss Fuse 7.9.0 has been installed and is running on EAP 7.4.0"
+  echo ">>> JBoss Fuse 7.9.0 has been installed on EAP 7.4.0"
   rm -Rf /root/install
+  echo ">>> Restart JBoss EAP Fuse Integration Server ..."
+  #nohup $EAP_HOME/bin/standalone.sh > $EAP_HOME/standalone/log/server.log &
+  #$EAP_HOME/bin/jboss-cli.sh --connect command=:stop
+  $EAP_HOME/bin/standalone.sh
+  waiting_for_jboss
 fi
